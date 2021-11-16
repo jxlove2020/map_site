39e36f3f5849a877afa61bb458528e914e9a0d6a
Joseph <jxlove2020@gmail.com>
2021-11-16 11:25:29 +0900

    서비스워커 로그 주석

--- service_worker.js
+++ service_worker.js
@@ -16,10 +16,10 @@ const aFilesToCache = [
 
 // 2.서비스워커를 설치하고 캐시를 저장함
 self.addEventListener('install', pEvent => {
-  console.log('서비스워커 설치함!');
+  // console.log('서비스워커 설치함!');
   pEvent.waitUntil(
     caches.open(sCacheName).then(pCache => {
-      console.log('파일을 캐시에 저장함!');
+      // console.log('파일을 캐시에 저장함!');
       return pCache.addAll(aFilesToCache);
     })
   );
@@ -27,7 +27,7 @@ self.addEventListener('install', pEvent => {
 
 // 3. 고유번호 할당받은 서비스 워커 동작 시작
 self.addEventListener('activate', pEvent => {
-  console.log('서비스워커 동작 시작됨!');
+  // console.log('서비스워커 동작 시작됨!');
 });
 
 // 4.데이터 요청시 네트워크 또는 캐시에서 찾아 반환
@@ -37,10 +37,10 @@ self.addEventListener('fetch', pEvent => {
       .match(pEvent.request)
       .then(response => {
         if (!response) {
-          console.log('네트워크에서 데이터 요청!', pEvent.request);
+          // console.log('네트워크에서 데이터 요청!', pEvent.request);
           return fetch(pEvent.request);
         }
-        console.log('캐시에서 데이터 요청!', pEvent.request);
+        // console.log('캐시에서 데이터 요청!', pEvent.request);
         return response;
       })
       .catch(err => console.log(err))
